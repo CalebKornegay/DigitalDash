fe9a552eac419f27fde637a8e0126f5b1117d177
CalebKornegay <caleb.kornegay@gmail.com>
2024-12-13 12:47:10 -0500

    Go funcs to update in background with separate timeouts

--- server/main.go
+++ server/main.go
@@ -10,10 +10,19 @@ import (
 )
 
 type DigitalDash struct {
-	rpm_wait        time.Duration
-	fuel_level_wait time.Duration
-	device          *elmobd.Device
-	wg              sync.WaitGroup
+	device               *elmobd.Device
+	wg                   sync.WaitGroup
+	rpm_wait             time.Duration
+	fuel_level_wait      time.Duration
+	coolant_temp_wait    time.Duration
+	engine_oil_temp_wait time.Duration
+	intake_air_temp_wait time.Duration
+	maf_wait             time.Duration
+	gear_wait            time.Duration
+	speed_wait           time.Duration
+	ambient_temp_wait    time.Duration
+	throttle_pos_wait    time.Duration
+	odometer_wait        time.Duration
 }
 
 func try_panic(err error) {
@@ -53,15 +62,150 @@ func (dash *DigitalDash) updateFuelLevel() {
 	dash.wg.Done()
 }
 
+func (dash *DigitalDash) updateCoolantTemp() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewCoolantTemperature()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The coolant temp is %d\u00b0\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateEngineOilTemp() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewEngineOilTemperature()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The engine oil temp is %d\u00b0\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateIntakeAirTemp() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewIntakeAirTemperature()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The intake air temp is %d\u00b0\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateMAFFlowRate() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewMafAirFlowRate()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The mass air flow sensor air flow rate is is %fL/hr\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateActualGear() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewTransmissionActualGear()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The current gear is %s\n", cmd.ValueAsLit())
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateSpeed() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewVehicleSpeed()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The current speed is %d\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateAmbientTemp() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewAmbientTemperature()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The ambient temp is %d\u00b0\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateThrottlePosition() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewThrottlePosition()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The throttle position is %f%%\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateOdometer() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewOdometer()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The current mileage is %f miles\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
 func main() {
 
 	device, err := elmobd.NewDevice("/dev/ttyUSB0", true)
 	try_panic(err)
 
 	var dash DigitalDash = DigitalDash{
-		device:          device,
-		rpm_wait:        time.Millisecond * 50,
-		fuel_level_wait: time.Second * 5,
+		device:               device,
+		rpm_wait:             time.Millisecond * 50,
+		fuel_level_wait:      time.Second * 5,
+		coolant_temp_wait:    time.Millisecond * 100,
+		engine_oil_temp_wait: time.Millisecond * 100,
+		intake_air_temp_wait: time.Millisecond * 100,
+		maf_wait:             time.Millisecond * 500,
+		gear_wait:            time.Second * 1,
+		speed_wait:           time.Millisecond * 50,
+		ambient_temp_wait:    time.Millisecond * 500,
+		throttle_pos_wait:    time.Millisecond * 500,
+		odometer_wait:        time.Second * 5,
 	}
 
 	// on, err := device.GetIgnitionState()
@@ -70,6 +214,15 @@ func main() {
 
 	go dash.updateRPM()
 	go dash.updateFuelLevel()
+	go dash.updateCoolantTemp()
+	go dash.updateEngineOilTemp()
+	go dash.updateIntakeAirTemp()
+	go dash.updateMAFFlowRate()
+	go dash.updateOdometer()
+	go dash.updateSpeed()
+	go dash.updateThrottlePosition()
+	go dash.updateActualGear()
+	go dash.updateAmbientTemp()
 
 	dash.wg.Wait()
 }
