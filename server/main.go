1e026fdb2d0ff8a4554e8b1e19dd5bf72f07091b
CalebKornegay <caleb.kornegay@gmail.com>
2024-12-13 12:30:11 -0500

    Go funcs to update in background with separate timeouts

--- server/main.go
+++ server/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/rzetterberg/elmobd"
 )
+
+type DigitalDash struct {
+	rpm_wait        time.Duration
+	fuel_level_wait time.Duration
+	device          *elmobd.Device
+	wg              sync.WaitGroup
+}
 
 func try_panic(err error) {
 	if err != nil {
@@ -15,26 +23,53 @@ func try_panic(err error) {
 	}
 }
 
-func main() {
-	time.Sleep(time.Second * 1)
+func (dash *DigitalDash) updateRPM() {
+	dash.wg.Add(1)
 
-	info, err := os.Stat("/dev/ttyUSB0")
-	try_panic(err)
-	fmt.Println(info)
+	for {
+		cmd := elmobd.NewEngineRPM()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The engine rpm is %f\n", cmd.Value)
+
+		time.Sleep(dash.rpm_wait)
+	}
+
+	dash.wg.Done()
+}
+
+func (dash *DigitalDash) updateFuelLevel() {
+	dash.wg.Add(1)
+
+	for {
+		cmd := elmobd.NewFuel()
+		_, err := dash.device.RunOBDCommand(cmd)
+
+		try_panic(err)
+		fmt.Printf("The fuel level is %f%%\n", cmd.Value)
+	}
+
+	dash.wg.Done()
+}
+
+func main() {
 
 	device, err := elmobd.NewDevice("/dev/ttyUSB0", true)
 	try_panic(err)
 
-	on, err := device.GetIgnitionState()
-	try_panic(err)
-	fmt.Printf("The car is on: %t\n", on)
+	var dash DigitalDash = DigitalDash{
+		device:          device,
+		rpm_wait:        time.Millisecond * 50,
+		fuel_level_wait: time.Second * 5,
+	}
 
-	rpm, err := device.RunOBDCommand(elmobd.NewEngineRPM())
-	try_panic(err)
-	fmt.Printf("The engine rpm is %s\n", rpm.ValueAsLit())
+	// on, err := device.GetIgnitionState()
+	// try_panic(err)
+	// fmt.Printf("The car is on: %t\n", on)
 
-	fuel, err := device.RunOBDCommand(elmobd.NewFuel())
-	try_panic(err)
-	fmt.Printf("The fuel level is %s\n", fuel.ValueAsLit())
+	go dash.updateRPM()
+	go dash.updateFuelLevel()
 
+	dash.wg.Wait()
 }
